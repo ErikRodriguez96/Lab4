2d320539ec0da4174587af828a68f8e487b90d74
ErikRodriguez96 <tuf93296@temple.edu>
2022-02-20 23:50:50 -0500

    Populated array of images, created layout for thumbnails

--- app/src/main/java/edu/temple/imageactivity/ImageActivity.kt
+++ app/src/main/java/edu/temple/imageactivity/ImageActivity.kt
@@ -2,11 +2,29 @@ package edu.temple.imageactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class ImageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val imageView = findViewById<ImageView>(R.id.LargeCityImageView)
+        val textView = findViewById<TextView>(R.id.CityTextView)
+
+        val cityPics = arrayOf(
+            ImageObject(R.drawable.amsterdam, "Amsterdam, NL"),
+            ImageObject(R.drawable.copenhagen, "Copenhagen, DK"),
+            ImageObject(R.drawable.florence, "Florence, IT"),
+            ImageObject(R.drawable.london, "London, UK"),
+            ImageObject(R.drawable.nyc, "New York City, US"),
+            ImageObject(R.drawable.paris, "Paris, FR"),
+            ImageObject(R.drawable.san_francisco, "San Francisco, UA"),
+            ImageObject(R.drawable.shenzhen, "Shenzhen, CN"),
+            ImageObject(R.drawable.singapore, "Singapore, MY"),
+            ImageObject(R.drawable.stockholm, "Stockholm, SE"),
+            ImageObject(R.drawable.tokyo, "Tokyo, JP")
+        )
     }
 }
--- app/src/main/java/edu/temple/imageactivity/ImageAdapter.kt
+++ app/src/main/java/edu/temple/imageactivity/ImageAdapter.kt
@@ -6,20 +6,20 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ImageAdapter : RecyclerView.Adapter<ImageAdapter.ViewHolder>(){
+class ImageAdapter(_images: Array<ImageObject>, _myRecyclerViewFunc: (ImageObject) -> Unit) : RecyclerView.Adapter<ImageAdapter.ImageViewHolder>(){
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+    class ImageViewHolder(view: View) : RecyclerView.ViewHolder(view){
         val textView : TextView
         init {
             textView = view.findViewById(R.id.CityTextView)
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
         TODO("Not yet implemented")
     }
 
-    override fun onBindViewHolder(holder: ImageAdapter.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ImageAdapter.ImageViewHolder, position: Int) {
         TODO("Not yet implemented")
     }
 
